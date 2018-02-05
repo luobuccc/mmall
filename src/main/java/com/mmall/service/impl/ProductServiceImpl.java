6d8a2cd697e5850a0e6b3e8b92a7ab3aa60d2a5b
LuoBu <532546122@qq.com>
2018-02-05 15:28:39 +0800

    7-结束

--- src/main/java/com/mmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/mmall/service/impl/ProductServiceImpl.java
@@ -117,7 +117,9 @@ public class ProductServiceImpl implements IProductService {
         if (StringUtils.isNotBlank(productName)) {
             productName = new StringBuilder().append("%").append(productName).append("%").toString();
         }
+
         List<Product> productList = productMapper.selectByNameAndProductId(productName, productId);
+
         List<ProductListVo> productListVoList = Lists.newArrayList();
         for (Product productItem : productList) {
             ProductListVo productListVo = assembleProductListVo(productItem);
