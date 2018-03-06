f614b3b7668d8da4757e96822b858dc155c54209
LuoBu <532546122@qq.com>
2018-03-06 14:06:19 +0800

    cookie util

---
+++ src/main/java/com/mmall/util/JsonUtil.java
@@ -0,0 +1,7 @@
+package com.mmall.util;
+
+/**
+ * Created by 53254 on 2018/3/6 12:03 /mmal
+ */
+public class JsonUtil {
+}
---
+++ src/main/java/com/mmall/util/RedisPoolUtil.java
@@ -0,0 +1,7 @@
+package com.mmall.util;
+
+/**
+ * Created by 53254 on 2018/3/6 10:44 /mmal
+ */
+public class RedisPoolUtil {
+}
