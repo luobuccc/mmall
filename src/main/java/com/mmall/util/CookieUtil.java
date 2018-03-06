d42c59cc9bd51fbdbb3b99a923c7df3011c31531
LuoBu <532546122@qq.com>
2018-03-06 15:12:02 +0800

    session

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -19,9 +19,8 @@ public class CookieUtil {
         Cookie[] cks = request.getCookies();
         if (cks != null) {
             for (Cookie ck : cks) {
-                log.info("cookieName:{},cookieValue{}", ck.getName(), ck.getValue());
                 if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
-                    log.info("return cookieName:{},cookieValue{}", ck.getName(), ck.getValue());
+                    log.info("return cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
                     return ck.getValue();
                 }
             }
@@ -33,6 +32,7 @@ public class CookieUtil {
         Cookie ck = new Cookie(COOKIE_NAME, token);
         ck.setDomain(COOKIE_DOMAIN);
         ck.setPath("/");//设置在根目录
+        ck.setHttpOnly(true);//防止脚本攻击
         ck.setMaxAge(60 * 60 * 24 * 365);//-1永久 单位 秒,如果不设置则不写入硬盘，只写入内存，只在当前页面有效
         log.info("write cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
         response.addCookie(ck);
@@ -42,12 +42,12 @@ public class CookieUtil {
         Cookie[] cks = request.getCookies();
         if (cks != null) {
             for (Cookie ck : cks) {
-                log.info("cookieName:{},cookieValue{}", ck.getName(), ck.getValue());
+                log.info("cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
                 if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
                     ck.setDomain(COOKIE_DOMAIN);
                     ck.setPath("/");
                     ck.setMaxAge(0);//删除此cookie
-                    log.info("del cookieName:{},cookieValue{}", ck.getName(), ck.getValue());
+                    log.info("del cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
                     response.addCookie(ck);
                     return;
                 }
